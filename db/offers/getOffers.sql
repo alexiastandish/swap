80740890eb95c27f959081781b0089af35aeaa93
alexiastandish <alexiastandish@gmail.com>
2018-10-23 22:10:13 -0500

    positco offers functioning

--- db/offers/getOffers.sql
+++ db/offers/getOffers.sql
@@ -4,7 +4,14 @@
 -- JOIN images im ON (im.imageurl_itemid = i.items_id )
 -- WHERE touserid = $1;
 
-SELECT o.*
-FROM offers o 
-JOIN items i ON (i.items_id = o.fromuser_itemid)
-WHERE touserid = $1 AND offer_status = 1 ;
+-- SELECT o.*
+-- FROM offers o 
+-- JOIN items i ON (i.items_id = o.fromuser_itemid)
+-- WHERE touserid = $1 AND offer_status = 1 ;
+
+select yi.item_name as your_item, ti.item_name as their_item, o.requesteditemid, o.fromuser_itemid, o.offer_id, o.offer_status, u.username
+from offers o
+inner join items yi on yi.items_id = o.requesteditemid 
+inner join items ti on ti.items_id = o.fromuser_itemid
+inner join users u on u.user_id = o.fromuserid
+where touserid = $1 AND offer_status = 1;
